b71a4dfe89593183568e6b488a5cc12c25d1381d
FYKO <fynn_buehler@hotmail.com>
2026-07-04 18:25:54 +0200

    Create data.js

---
+++ data.js
@@ -0,0 +1,23 @@
+const PRODUCTS = [
+  {
+    id: 1,
+    name: "FYKO ESSENTIAL TEE BLACK",
+    price: 23,
+    desc: "Heavy cotton oversized fit",
+    img: "https://via.placeholder.com/500x500"
+  },
+  {
+    id: 2,
+    name: "FYKO ESSENTIAL TEE WHITE",
+    price: 23,
+    desc: "Clean minimal street silhouette",
+    img: "https://via.placeholder.com/500x500"
+  },
+  {
+    id: 3,
+    name: "FYKO ESSENTIAL TEE GREY",
+    price: 23,
+    desc: "Everyday wear street essential",
+    img: "https://via.placeholder.com/500x500"
+  }
+];
